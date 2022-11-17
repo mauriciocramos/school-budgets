2bb092ea3a283533722a0c2e2255652b4a5365b5
Maurício Collaça <mauriciocramos@gmail.com>
2022-11-17 15:11:03 -0300

    minor changes

--- optimize-sample-split.py
+++ optimize-sample-split.py
@@ -24,22 +24,22 @@ sample_size_space = np.concatenate([np.linspace(1.0, 0.1, 10), np.linspace(0.09,
 # sample min_count space limited by the population less frequent class
 sample_min_count_space = range(0, y.sum().min() + 1)
 
-for sample_size in sample_size_space: # 19 iterations
-    for sample_min_count in sample_min_count_space: # 30 iterations
-        sample_df = multilabel_sample_dataframe(df[LABELS], # avoiding features as it's just a simulation on labels
+for sample_size in sample_size_space:  # 19 iterations
+    for sample_min_count in sample_min_count_space:  # 30 iterations
+        sample_df = multilabel_sample_dataframe(df[LABELS],  # no features as it's just a simulation
                                                 y, size=sample_size, min_count=sample_min_count, seed=1)
-        dummy_labels = pd.get_dummies(sample_df[LABELS], prefix_sep='__')
-        sample_ratio = (dummy_labels.value_counts() / y_counts).mean()
+        sample_labels = pd.get_dummies(sample_df[LABELS], prefix_sep='__')
+        sample_ratio = (sample_labels.value_counts() / y_counts).mean()
         # The sample less frequent class upper limits the split min_count otherwise it fails
-        sample_less_frequent_class = dummy_labels.sum().min()
+        sample_less_frequent_class = sample_labels.sum().min()
         for test_size in np.linspace(0.1, 0.9, 9):
             test_min_count_space = range(0, sample_less_frequent_class + 1)
             for test_min_count in test_min_count_space:
-                # The test_size lower limits the test_min_count
-                if dummy_labels.shape[1] * test_min_count > np.floor(dummy_labels.shape[0] * test_size):
+                # The test_size limits the test_min_count
+                if sample_labels.shape[1] * test_min_count > np.floor(sample_labels.shape[0] * test_size):
                     continue
                 X_train, X_test, y_train, y_test = multilabel_train_test_split(sample_df[[]],
-                                                                               dummy_labels,
+                                                                               sample_labels,
                                                                                size=test_size,
                                                                                min_count=test_min_count,
                                                                                seed=1)
@@ -47,8 +47,8 @@ for sample_size in sample_size_space: # 19 iterations
                 test_ratio = (y_test.value_counts() / y_counts).mean()
                 train_lessfreq = y_train.sum().min()
                 test_lessfreq = y_test.sum().min()
-                msg = 'sample_size {:.2f} min_count {} less_freq {} sample_ratio {:.5f} test_size {:.1f} min_count {} train_less_freq {} test_lessfreq {} ' \
-                      'train_ratio {:.5f} test_ratio {:.5f}'
+                msg = 'sample_size {:.2f} min_count {} less_freq {} sample_ratio {:.5f} test_size {:.1f} min_count {} '\
+                      'train_less_freq {} test_lessfreq {} train_ratio {:.5f} test_ratio {:.5f}'
                 print(msg.format(sample_size, sample_min_count, sample_less_frequent_class, sample_ratio, test_size,
                                  test_min_count, train_lessfreq, test_lessfreq, train_ratio, test_ratio))
                 results = pd.concat([results,
